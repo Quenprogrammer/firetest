15a9f23516a11908a0044cb8e9ea3999d4702f14
QuenProgrammer <adamsadam3667@gmail.com>
2024-01-18 06:14:38 -0800

    removed almost all the components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,7 @@
 import { Routes } from '@angular/router';
-import {CoursesComponent} from "./courses/courses.component";
-import {InstructorsComponent} from "./instructors/instructors.component";
-import {ProjectsComponent} from "./projects/projects.component";
 import {RegisterComponent} from "./register/register.component";
-import {ProfileEditorComponent} from "./profile-editor/profile-editor.component";
 
 export const routes: Routes = [
-  { path: 'courses', component: CoursesComponent },
-  { path: 'instructors', component: InstructorsComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'projects', component: ProjectsComponent },
-  { path: 'testing', component: ProfileEditorComponent },
+
 ];
